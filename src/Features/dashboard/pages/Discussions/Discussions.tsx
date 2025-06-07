a4af5626692ec963932244eb50fe89578c055033
MahmoudQasem <mahmoudqasem1@hotmail.com>
2025-06-07 06:37:45 +0300

    upload Discussions Page

--- src/Features/dashboard/pages/Discussions/Discussions.tsx
+++ src/Features/dashboard/pages/Discussions/Discussions.tsx
@@ -1,8 +1,15 @@
 import {FC, JSX} from 'react';
+import SectionTitle from '../../components/SectionTitle/SectionTitle';
 
 const Discussions: FC = (): JSX.Element => {
   return (
-    <div>Discussions</div>
+    <>
+    <section className="bg-gray-200 px-4 py-6 rounded-2xl mx-2 mb-6">
+        <div className="flex items-center justify-center mb-4">
+          <SectionTitle title="Discussions" />
+        </div>
+      </section>
+    </>
   )
 }
 
